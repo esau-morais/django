ef24e5b1f4cc45c82ff5f9119d0e57bd7d66159a
esau-morais <esaumorais7@gmail.com>
2020-03-31 16:41:15 -0300

    heroku added

--- Django/settings.py
+++ Django/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,7 +34,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'heroku',
     'website.apps.WebsiteConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
