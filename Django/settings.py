53dcdef06198d8da893963fa76fcd795c4efa86e
esau-morais <esaumorais7@gmail.com>
2020-04-04 10:48:44 -0300

    whitenoise updated

--- Django/settings.py
+++ Django/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
